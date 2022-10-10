2a1729cc77b2cee87eadbf580e740e9f24f97008
Anson He <ansonjwhe@gmail.com>
2022-10-09 23:28:37 -0400

    implement basic auth system

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from 'react'
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import { AuthProvider } from "./contexts/AuthContext"
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+
+import './App.css';
 
 // import MainContent from "./components/MainContent";
 // import ClaimSummary from "./components/ClaimSummary";
-import CreateUser from './components/CreateUser'
+import Login from "./components/Login";
+import CreateUser from "./components/CreateUser";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" exact element={<CreateUser />} />
-                {/* <Route path="/claim" element={ClaimSummary} /> */}
-                <Route path="/user" element={<CreateUser />} />
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+            <Route exact path="/" element={<Navigate replace to="/login" />} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/user" element={<CreateUser/>} />
+            {/* <Route path="/claim" element={ClaimSummary} /> */}
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App
---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,47 @@
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth'
+import React, { useContext, useEffect, useState } from 'react'
+import app from '../firebase'
+
+const AuthContext = React.createContext()
+
+export const useAuth = () => {
+    return useContext(AuthContext)
+}
+
+export const AuthProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState()
+    const [loading, setLoading] = useState(true)
+    const auth = getAuth(app)
+    
+    const provider = new GoogleAuthProvider()
+    provider.setCustomParameters({prompt: 'select_account'})
+
+    const login = () => {
+        return signInWithPopup(auth, provider)
+    }
+
+    const logout = () => {
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setCurrentUser(user)
+            setLoading(false)
+        })
+
+        return unsubscribe
+    }, [auth])
+
+    const value = {
+        currentUser,
+        login,
+        logout
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    )
+}
