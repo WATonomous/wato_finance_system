a606169561e66c44f44b94b83548db4b9ba62f22
Victor Zheng <vzheng@gmail.com>
2022-10-29 21:04:55 -0400

    delete_to_force

--- backend/routes/emails.routes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-const EmailsController = require('../controllers/emails.controller')
-
-router.route('/').post(EmailsController.createEmail)
-
-module.exports = router
