27981aeb7e936eaf8d4ca514cf418fc3d023e24a
Victor Zheng <vzheng@gmail.com>
2022-11-05 18:04:43 -0400

    updated readme

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -7,9 +7,8 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+    // measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 }
-console.log(firebaseConfig)
 
 const app = initializeApp(firebaseConfig)
 
