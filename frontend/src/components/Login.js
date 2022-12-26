8991d2fa89db92593dfedcb5d60c9b9c8558cf8c
Anson He <ansonjwhe@gmail.com>
2022-12-26 18:48:02 -0500

    run prettier

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,8 +2,12 @@ import React, { useState } from 'react'
 import { useAuth } from '../contexts/AuthContext'
 import { useNavigate } from 'react-router-dom'
 import Navbar from './Navbar'
-import { Center, Heading, keyframes, usePrefersReducedMotion } from '@chakra-ui/react'
-
+import {
+    Center,
+    Heading,
+    keyframes,
+    usePrefersReducedMotion,
+} from '@chakra-ui/react'
 
 const Login = () => {
     const [error, setError] = useState('')
@@ -37,14 +41,26 @@ const Login = () => {
 
     return (
         <>
-            <Navbar onClick={handleLogin} authButtonText="Log In" authButtonDisabled={loading}/>
-            <Center 
-                h='calc(100vh - 80px)'
-                bgGradient='linear(to-tl, #ee7752, #e73c7e, #23a6d5, #23d5ab)'
-                bgSize='400% 400%'
-                animation={prefersReducedMotion ? undefined : `${bgKeyframe} 15s ease infinite`}
+            <Navbar
+                onClick={handleLogin}
+                authButtonText="Log In"
+                authButtonDisabled={loading}
+            />
+            <Center
+                h="calc(100vh - 80px)"
+                bgGradient="linear(to-tl, #ee7752, #e73c7e, #23a6d5, #23d5ab)"
+                bgSize="400% 400%"
+                animation={
+                    prefersReducedMotion
+                        ? undefined
+                        : `${bgKeyframe} 15s ease infinite`
+                }
             >
-                <Heading>{error ? error : 'Log in to access the WATonomous Finance System'}</Heading>
+                <Heading>
+                    {error
+                        ? error
+                        : 'Log in to access the WATonomous Finance System'}
+                </Heading>
             </Center>
         </>
     )
--- frontend/src/components/TicketList.js
+++ frontend/src/components/TicketList.js
@@ -5,43 +5,61 @@ import { useNavigate } from 'react-router-dom'
 const TicketList = (props) => {
     const navigate = useNavigate()
 
-    const sfCardData = props.sfData.map(sf => ({'name': `${sf.organization} - ${sf.semester}`, 'type': 'SF', 'id': sf._id}))
-    const fiCardData = props.fiData.map(fi => ({'name': fi.name, 'type': 'FI', 'id': fi._id}))
-    const pprCardData = props.pprData.map(ppr => ({'name': ppr.name, 'type': 'PPR', 'id': ppr._id}))
-    const uprCardData = props.uprData.map(upr => ({'name': upr.name, 'type': 'UPR', 'id': upr._id}))
-    const allTickets = [].concat(sfCardData, fiCardData, pprCardData, uprCardData)
+    const sfCardData = props.sfData.map((sf) => ({
+        name: `${sf.organization} - ${sf.semester}`,
+        type: 'SF',
+        id: sf._id,
+    }))
+    const fiCardData = props.fiData.map((fi) => ({
+        name: fi.name,
+        type: 'FI',
+        id: fi._id,
+    }))
+    const pprCardData = props.pprData.map((ppr) => ({
+        name: ppr.name,
+        type: 'PPR',
+        id: ppr._id,
+    }))
+    const uprCardData = props.uprData.map((upr) => ({
+        name: upr.name,
+        type: 'UPR',
+        id: upr._id,
+    }))
+    const allTickets = [].concat(
+        sfCardData,
+        fiCardData,
+        pprCardData,
+        uprCardData
+    )
 
     return (
         <VStack
-            pos='absolute'
-            left='0'
-            w='300px'
-            h='calc(100vh - 80px)'
-            overflowY='scroll'
+            pos="absolute"
+            left="0"
+            w="300px"
+            h="calc(100vh - 80px)"
+            overflowY="scroll"
         >
-            {allTickets.map(ticket => {
-                    const code = `${ticket.type}-${ticket.id}`
-                    return (
-                        <Card
-                            key={code}
-                            w='100%'
-                            borderTop='1px solid #dedede'
-                            borderBottom='1px solid #dedede'
-                            borderRadius='0'
-                            mt='0 !important'
-                            onClick={() => navigate(`/${ticket.type}/${ticket.id}`)}
-                            cursor='pointer'
-                        >
-                            <CardBody
-                                p='8px 16px'
-                            >
-                                <Text>{code}</Text>
-                                <Text>{ticket.name}</Text>
-                            </CardBody>
-                        </Card>
-                    )
-                }
-            )}
+            {allTickets.map((ticket) => {
+                const code = `${ticket.type}-${ticket.id}`
+                return (
+                    <Card
+                        key={code}
+                        w="100%"
+                        borderTop="1px solid #dedede"
+                        borderBottom="1px solid #dedede"
+                        borderRadius="0"
+                        mt="0 !important"
+                        onClick={() => navigate(`/${ticket.type}/${ticket.id}`)}
+                        cursor="pointer"
+                    >
+                        <CardBody p="8px 16px">
+                            <Text>{code}</Text>
+                            <Text>{ticket.name}</Text>
+                        </CardBody>
+                    </Card>
+                )
+            })}
         </VStack>
     )
 }
