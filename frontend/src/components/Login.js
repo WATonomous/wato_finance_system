404705f1dc453191398e1dec6b357914b8f3baee
Anson He <ansonjwhe@gmail.com>
2022-12-28 22:52:36 -0500

    remove login page animation

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,31 +2,13 @@ import React, { useState } from 'react'
 import { useAuth } from '../contexts/AuthContext'
 import { useNavigate } from 'react-router-dom'
 import Navbar from './Navbar'
-import {
-    Center,
-    Heading,
-    keyframes,
-    usePrefersReducedMotion,
-} from '@chakra-ui/react'
+import { Center, Heading } from '@chakra-ui/react'
 
 const Login = () => {
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
     const { login } = useAuth()
     const navigate = useNavigate()
-    const prefersReducedMotion = usePrefersReducedMotion()
-
-    const bgKeyframe = keyframes`
-        0% {
-            background-position: 0% 50%;
-        }
-        50% {
-            background-position: 100% 50%;
-        }
-        100% {
-            background-position: 0% 50%;
-        }
-    `
 
     const handleLogin = async () => {
         setLoading(true)
@@ -48,13 +30,7 @@ const Login = () => {
             />
             <Center
                 h="calc(100vh - 80px)"
-                bgGradient="linear(to-tl, #ee7752, #e73c7e, #23a6d5, #23d5ab)"
-                bgSize="400% 400%"
-                animation={
-                    prefersReducedMotion
-                        ? undefined
-                        : `${bgKeyframe} 15s ease infinite`
-                }
+                bgGradient="linear(to-tl, #23a6d5, #23d5ab)"
             >
                 <Heading>
                     {error
