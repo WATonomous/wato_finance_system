863431e5efbf5e4c788e992fefed8ef4e59cc6d1
Anson He <ansonjwhe@gmail.com>
2022-12-28 23:38:55 -0500

    add onclick for navbar title to navigate to dashboard homepage

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,14 @@
-import { Button, Flex, Heading, Spacer } from '@chakra-ui/react'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Button, Flex, Heading, Spacer } from '@chakra-ui/react'
+
 
 const Navbar = (props) => {
+    const navigate = useNavigate()
+
     return (
         <Flex alignItems="center" p="16px 24px" bgColor="deepskyblue">
-            <Heading lineHeight="48px">WATonomous Finance System</Heading>
+            <Heading lineHeight="48px" onClick={() => navigate('/')} cursor="pointer">WATonomous Finance System</Heading>
             <Spacer />
             <Button onClick={props.onClick} disabled={props.authButtonDisabled}>
                 {props.authButtonText}
