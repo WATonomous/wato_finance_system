a7315fdf361a48d77439d5e67ca7d345d0427b60
NicholasLin718 <nlin311@gmail.com>
2023-01-07 12:05:12 -0500

    refactored code and repositioned ticket list in dashboard

--- frontend/src/components/TreeView.js
+++ frontend/src/components/TreeView.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 import { Text, Box } from '@chakra-ui/react'
 import getAllChildren from '../utils/getAllChildren'
+import { TICKET_TYPES } from '../pages/Dashboard'
 
 const TreeView = (props) => {
     const navigate = useNavigate()
@@ -12,19 +13,12 @@ const TreeView = (props) => {
     const ticketType = pathname[0]
     const ticketID = parseInt(pathname[1])
 
-    const TICKET_TYPES = Object.freeze({
-        SF: 'SF',
-        FI: 'FI',
-        PPR: 'PPR',
-        UPR: 'UPR',
-    })
-
-    const sortTickets = (list) => {
-        let sortedArr = list.sort((a, b) => (a.name > b.name ? 1 : -1))
+    const sortTickets = (ticketList) => {
+        let sortedArr = ticketList.sort((a, b) => (a._id > b._id ? 1 : -1))
         return sortedArr
     }
 
-    let { SF, FI, PPR, UPR } = tickets
+    const { SF, FI, PPR, UPR } = tickets
     useEffect(() => {
         setTree({})
         if (SF.length === 0) return
@@ -35,143 +29,137 @@ const TreeView = (props) => {
                 id = FI.find((ticket) => ticket._id === ticketID).sf_link
                 break
             case TICKET_TYPES.PPR:
-                let ppr = PPR.find((ticket) => ticket._id === ticketID)
+                const ppr = PPR.find((ticket) => ticket._id === ticketID)
                 id = FI.find((ticket) => ticket._id === ppr.fi_link).sf_link
                 break
             case TICKET_TYPES.UPR:
-                let upr = UPR.find((ticket) => ticket._id === ticketID)
+                const upr = UPR.find((ticket) => ticket._id === ticketID)
                 id = FI.find((ticket) => ticket._id === upr.fi_link).sf_link
                 break
         }
         if (id !== -1) setTree(getAllChildren(id, tickets))
     }, [tickets, location])
 
+    if (location.pathname === '/' || Object.keys(tree).length === 0) return null
+
     return (
         <Box
-            pos="fixed"
-            bottom="0"
-            right="0"
-            width="300px"
             border="1px"
             borderColor="gray.200"
             height="auto"
+            paddingY="24px"
+            pl="24px"
+            pr="56px"
         >
-            {location.pathname !== '/' && Object.keys(tree).length !== 0 && (
-                <Box padding="24px" width="250px">
-                    <Box bgColor="tomato" cursor="pointer">
-                        <Text
-                            as={
-                                ticketType === 'SF' && ticketID === tree._id
-                                    ? 'b'
-                                    : ''
-                            }
-                            fontSize="xs"
-                            onClick={() => navigate(`/SF/${tree._id}`)}
-                        >
-                            {tree.name}
-                        </Text>
-                    </Box>
-
-                    {sortTickets(tree.fundingItems).map((fi, i) => {
-                        return (
-                            <Box key={i}>
-                                <Box
-                                    pos="relative"
-                                    left="4"
-                                    bgColor="#34cceb"
-                                    cursor="pointer"
-                                >
-                                    <Text
-                                        as={
-                                            ticketType ===
-                                                `${TICKET_TYPES.FI}` &&
-                                            ticketID === fi._id
-                                                ? 'b'
-                                                : ''
-                                        }
-                                        fontSize="xs"
-                                        onClick={() =>
-                                            navigate(
-                                                `/${TICKET_TYPES.FI}/${fi._id}`
-                                            )
-                                        }
-                                    >
-                                        {fi.name}
-                                    </Text>
-                                </Box>
-                                {sortTickets(fi.personalPurchases).map(
-                                    (ppr, j) => {
-                                        return (
-                                            <Box
-                                                key={j}
-                                                pos="relative"
-                                                left="8"
-                                                border="1px"
-                                                borderColor="gray.200"
-                                                borderRadius="4"
-                                                cursor="pointer"
-                                                bgColor="#22f5a1"
-                                            >
-                                                <Text
-                                                    as={
-                                                        ticketType ===
-                                                            `${TICKET_TYPES.PPR}` &&
-                                                        ticketID === ppr._id
-                                                            ? 'b'
-                                                            : ''
-                                                    }
-                                                    fontSize="xs"
-                                                    onClick={() =>
-                                                        navigate(
-                                                            `/${TICKET_TYPES.PPR}/${ppr._id}`
-                                                        )
-                                                    }
-                                                >
-                                                    {ppr.name}
-                                                </Text>
-                                            </Box>
-                                        )
-                                    }
-                                )}
-                                {sortTickets(fi.uwFinancePurchases).map(
-                                    (upr, k) => {
-                                        return (
-                                            <Box
-                                                key={k}
-                                                pos="relative"
-                                                left="8"
-                                                border="1px"
-                                                borderColor="gray.200"
-                                                borderRadius="4"
-                                                cursor="pointer"
-                                                bgColor="#ff87f3"
-                                            >
-                                                <Text
-                                                    as={
-                                                        ticketType ===
-                                                            `${TICKET_TYPES.UPR}` &&
-                                                        ticketID === upr._id
-                                                            ? 'b'
-                                                            : ''
-                                                    }
-                                                    fontSize="xs"
-                                                    onClick={() =>
-                                                        navigate(
-                                                            `/${TICKET_TYPES.UPR}/${upr._id}`
-                                                        )
-                                                    }
-                                                >
-                                                    {upr.name}
-                                                </Text>
-                                            </Box>
-                                        )
-                                    }
-                                )}
-                            </Box>
-                        )
-                    })}
+            <Box>
+                <Box bgColor="tomato" cursor="pointer">
+                    <Text
+                        as={
+                            ticketType === 'SF' && ticketID === tree._id
+                                ? 'b'
+                                : ''
+                        }
+                        fontSize="xs"
+                        onClick={() => navigate(`/SF/${tree._id}`)}
+                    >
+                        {`${TICKET_TYPES.SF} - ${tree.name}`}
+                    </Text>
                 </Box>
-            )}
+
+                {sortTickets(tree.fundingItems).map((fi) => {
+                    return (
+                        <Box key={`${TICKET_TYPES.FI} - ${fi.name}`}>
+                            <Box
+                                pos="relative"
+                                left="4"
+                                bgColor="#34cceb"
+                                cursor="pointer"
+                            >
+                                <Text
+                                    as={
+                                        ticketType === `${TICKET_TYPES.FI}` &&
+                                        ticketID === fi._id
+                                            ? 'b'
+                                            : ''
+                                    }
+                                    fontSize="xs"
+                                    onClick={() =>
+                                        navigate(
+                                            `/${TICKET_TYPES.FI}/${fi._id}`
+                                        )
+                                    }
+                                >
+                                    {`${TICKET_TYPES.FI} - ${fi.name}`}
+                                </Text>
+                            </Box>
+                            {sortTickets(fi.personalPurchases).map((ppr) => {
+                                return (
+                                    <Box
+                                        key={`${TICKET_TYPES.PPR} - ${ppr.name}`}
+                                        pos="relative"
+                                        left="8"
+                                        border="1px"
+                                        borderColor="gray.200"
+                                        borderRadius="4"
+                                        cursor="pointer"
+                                        bgColor="#22f5a1"
+                                    >
+                                        <Text
+                                            as={
+                                                ticketType ===
+                                                    `${TICKET_TYPES.PPR}` &&
+                                                ticketID === ppr._id
+                                                    ? 'b'
+                                                    : ''
+                                            }
+                                            fontSize="xs"
+                                            onClick={() =>
+                                                navigate(
+                                                    `/${TICKET_TYPES.PPR}/${ppr._id}`
+                                                )
+                                            }
+                                        >
+                                            {`${TICKET_TYPES.PPR} - ${ppr.name}`}
+                                        </Text>
+                                    </Box>
+                                )
+                            })}
+                            {sortTickets(fi.uwFinancePurchases).map((upr) => {
+                                return (
+                                    <Box
+                                        key={`${TICKET_TYPES.UPR} - ${upr.name}`}
+                                        pos="relative"
+                                        left="8"
+                                        border="1px"
+                                        borderColor="gray.200"
+                                        borderRadius="4"
+                                        cursor="pointer"
+                                        bgColor="#ff87f3"
+                                    >
+                                        <Text
+                                            as={
+                                                ticketType ===
+                                                    `${TICKET_TYPES.UPR}` &&
+                                                ticketID === upr._id
+                                                    ? 'b'
+                                                    : ''
+                                            }
+                                            fontSize="xs"
+                                            onClick={() =>
+                                                navigate(
+                                                    `/${TICKET_TYPES.UPR}/${upr._id}`
+                                                )
+                                            }
+                                        >
+                                            {`${TICKET_TYPES.UPR} - ${upr.name}`}
+                                        </Text>
+                                    </Box>
+                                )
+                            })}
+                        </Box>
+                    )
+                })}
+            </Box>
         </Box>
     )
 }
