3773f0c69804cd43e1c9e21c44fca930239819a6
NicholasLin718 <nlin311@gmail.com>
2023-01-12 00:25:10 -0500

    added endpoint for getting all users using firebase admin

---
+++ backend/controllers/users.controller.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+// import service account file (helps to know the firebase project details)
+const serviceAccount = require('../serviceAccountKey.json')
+const { getAuth } = require('firebase-admin/auth')
+
+// Intialize the firebase-admin project/account
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+})
+
+const auth = admin.auth()
+const getAllUsers = (_, res) => {
+    //Default retrieves 1000 users
+    auth.listUsers()
+        .then((userRecords) => {
+            res.status(200).json(userRecords.users)
+        })
+        .catch((error) => console.log(error))
+}
+
+module.exports = {
+    getAllUsers: functions.https.onRequest(getAllUsers),
+}
