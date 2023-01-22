c4f11f0d202c13fff1cbf3fabd2cc73ddfd6e0b9
NicholasLin718 <nlin311@gmail.com>
2023-01-22 15:28:01 -0500

    undo changes

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,45 +1,23 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { Center, Heading } from '@chakra-ui/react'
-
-import { useAuth } from '../contexts/AuthContext'
+import React from 'react'
+import { Center, Heading, VStack } from '@chakra-ui/react'
 import Navbar from '../components/Navbar'
 
 const Login = () => {
-    const [error, setError] = useState('')
-    const [loading, setLoading] = useState(false)
-    const { login } = useAuth()
-    const navigate = useNavigate()
-
-    const handleLogin = async () => {
-        setLoading(true)
-        try {
-            await login()
-            navigate('/user')
-        } catch (err) {
-            setError(`Failed to log in, Error: ${err}`)
-        }
-        setLoading(false)
-    }
-
     return (
-        <>
-            <Navbar
-                onClick={handleLogin}
-                authButtonText="Log In"
-                authButtonDisabled={loading}
-            />
+        <VStack spacing="0">
+            <Navbar />
             <Center
+                pos="absolute"
+                top="80px"
+                w="100%"
                 h="calc(100vh - 80px)"
                 bgGradient="linear(to-tl, #23a6d5, #23d5ab)"
             >
                 <Heading>
-                    {error
-                        ? error
-                        : 'Log in to access the WATonomous Finance System'}
+                    Log in to access the WATonomous Finance System
                 </Heading>
             </Center>
-        </>
+        </VStack>
     )
 }
 
