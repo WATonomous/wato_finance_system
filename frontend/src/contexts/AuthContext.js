0f127abbeae78e1daa36061d5bbb95a2712e79c9
NicholasLin718 <nlin311@gmail.com>
2023-01-21 23:40:40 -0500

    added currentUserGroup that is fetched from db

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -8,6 +8,8 @@ import {
 import React, { useContext, useEffect, useState } from 'react'
 import { useOutlet } from 'react-router-dom'
 import app from '../firebase'
+import axios from 'axios'
+import { useToast } from '@chakra-ui/react'
 
 const AuthContext = React.createContext()
 
@@ -17,24 +19,80 @@ export const useAuth = () => {
 
 export const AuthProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState()
+    const [currentUserGroup, setCurrentUserGroup] = useState()
     const [loading, setLoading] = useState(true)
     const auth = getAuth(app)
-
+    const whitelist = []
     const provider = new GoogleAuthProvider()
     provider.setCustomParameters({ prompt: 'select_account' })
+    const toast = useToast()
+
+    const showToast = () =>
+        toast({
+            title: 'Invalid email address',
+            description:
+                'Please use a WATonomous or whitelisted email address.',
+            status: 'error',
+            duration: 4000,
+            isClosable: true,
+        })
 
     const login = () => {
         return signInWithPopup(auth, provider)
+            .then((result) => {
+                const { email } = result.user
+                if (whitelist.includes(email)) return
+                if (!email.endsWith('@watonomous.ca')) {
+                    signOut(auth)
+                    showToast()
+                    throw new Error('Invalid email domain')
+                }
+            })
+            .catch((error) => {
+                console.error(error)
+            })
     }
 
     const logout = () => {
         return signOut(auth)
     }
 
+    const setUserGroup = async (user) => {
+        if (user) {
+            const userEmail = user.email
+            const userWatiam = userEmail.substring(0, userEmail.indexOf('@'))
+            const endpoint = `${process.env.REACT_APP_BACKEND_URL}/group/`
+
+            let searchWithEmail = false
+            if (whitelist.includes(userEmail)) searchWithEmail = true //or else use watiam
+
+            try {
+                const retrievedGroup = await axios.post(endpoint, {
+                    field: searchWithEmail ? userEmail : userWatiam,
+                    useEmail: searchWithEmail,
+                })
+                console.log(retrievedGroup.data.title)
+                setCurrentUserGroup(retrievedGroup.data.title)
+            } catch (err) {
+                console.log('Error: ' + err)
+            }
+        }
+    }
+
     useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                const email = user.email
+                if (
+                    !email.endsWith('@watonomous.ca') &&
+                    !whitelist.includes(email)
+                )
+                    return
+            }
             setCurrentUser(user)
             setLoading(false)
+
+            await setUserGroup(user)
         })
 
         return unsubscribe
@@ -42,6 +100,7 @@ export const AuthProvider = ({ children }) => {
 
     const value = {
         currentUser,
+        currentUserGroup,
         login,
         logout,
     }
