e58f4b28322b0c703f0a015b1e66ebb0aa75e07f
NicholasLin718 <nlin311@gmail.com>
2023-02-03 00:37:17 -0500

    added sort by timestamp

--- frontend/src/components/CommentsDisplay.js
+++ frontend/src/components/CommentsDisplay.js
@@ -11,13 +11,26 @@ const CommentsDisplay = (props) => {
         return user
     }
 
+    //mongoDB most likely already sorted by timestamp, but just in case
+    const sortMessagesByTimestamp = (comments) => {
+        comments.sort((a, b) => {
+            let dateA = new Date(a.createdAt)
+            let dateB = new Date(b.createdAt)
+            if (dateA.getTime() < dateB.getTime()) {
+                return -1
+            } else {
+                return 1
+            }
+        })
+        return comments
+    }
     useEffect(() => {
         const fetchComments = async () => {
             try {
                 const response = await axios.get(
                     `${process.env.REACT_APP_BACKEND_URL}/comments/${ticketType}/${ticketId}`
                 )
-                setAllComments(response.data)
+                setAllComments(sortMessagesByTimestamp(response.data))
             } catch (error) {
                 console.log(error)
             }
