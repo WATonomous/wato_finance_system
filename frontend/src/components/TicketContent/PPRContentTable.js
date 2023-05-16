0a3da238511868a88d032562a216e92c5f7030a9
Victor Zheng <36215359+victorzheng02@users.noreply.github.com>
2023-05-16 19:41:18 -0400

    add amount reimbursed field (#93)
    See https://github.com/WATonomous/wato_finance_system/issues/48

--- frontend/src/components/TicketContent/PPRContentTable.js
+++ frontend/src/components/TicketContent/PPRContentTable.js
@@ -3,8 +3,7 @@ import React from 'react'
 import { getFormattedCurrency } from '../../utils/utils'
 import TicketContentTableRow from './TicketContentTableRow'
 
-const PPRContentTable = (props) => {
-    const { ticketData } = props
+const PPRContentTable = ({ ticketData }) => {
     return (
         <VStack>
             <Table>
