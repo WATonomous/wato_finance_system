dd732be27bc91884ca6c3d018f80698a14a79648
Anson He <60114875+ansonjwhe@users.noreply.github.com>
2023-05-18 22:56:49 -0400

    Refactor Tree View (#97)

--- frontend/src/components/TreeView.js
+++ frontend/src/components/TreeView.js
@@ -1,29 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
-import { Text, Box } from '@chakra-ui/react'
+import { Text, Box, Stack } from '@chakra-ui/react'
 import getAllChildren from '../utils/getAllChildren'
 import { TICKET_TYPES } from '../pages/Dashboard'
 
-const TreeView = (props) => {
+const TreeView = ({ allTickets, currentTicket }) => {
     const navigate = useNavigate()
     const location = useLocation()
-    const { tickets } = props
     const [tree, setTree] = useState({})
 
-    const pathname = location.pathname.substring(1).split('/')
-    const ticketType = pathname[0]
-    const ticketID = parseInt(pathname[1])
-
     const sortTickets = (ticketList) => {
-        let sortedArr = ticketList.sort((a, b) => (a._id > b._id ? 1 : -1))
-        return sortedArr
+        return ticketList.sort((a, b) => (a._id > b._id ? 1 : -1))
     }
 
-    const { SF, FI, PPR, UPR } = tickets
     useEffect(() => {
-        setTree({})
-        if (SF.length === 0) return
+        const pathname = location.pathname.substring(1).split('/')
+        const ticketType = pathname[0]
+        const ticketID = parseInt(pathname[1])
+        const { SF, FI, PPR, UPR } = allTickets
 
+        if (SF.length === 0) return
         let id = ticketID
         switch (ticketType) {
             case TICKET_TYPES.FI:
@@ -37,121 +33,109 @@ const TreeView = (props) => {
                 const upr = UPR.find((ticket) => ticket._id === ticketID)
                 id = FI.find((ticket) => ticket._id === upr.fi_link).sf_link
                 break
+            default:
+                break
         }
-        if (id !== -1) setTree(getAllChildren(id, tickets))
-    }, [tickets, location])
+        setTree(getAllChildren(id, allTickets))
+    }, [location.pathname, allTickets])
 
-    if (location.pathname === '/' || Object.keys(tree).length === 0) return null
+    if (location.pathname === '/' || !tree || Object.keys(tree).length === 0)
+        return <Text>No tree to display</Text>
 
     return (
-        <Box
-            border="1px"
-            borderColor="gray.200"
-            height="auto"
-            paddingY="24px"
-            pl="24px"
-            pr="56px"
-        >
-            <Box>
-                <Box bgColor="tomato" cursor="pointer">
-                    <Text
-                        as={
-                            ticketType === tree.type && ticketID === tree._id
-                                ? 'b'
-                                : ''
-                        }
-                        fontSize="xs"
-                        onClick={() => navigate(tree.path)}
-                    >
-                        {`${tree.type} - ${tree.name}`}
-                    </Text>
-                </Box>
-
-                {sortTickets(tree.fundingItems).map((fi) => {
-                    return (
-                        <Box key={fi.code}>
-                            <Box
-                                pos="relative"
-                                left="4"
-                                bgColor="#34cceb"
-                                cursor="pointer"
-                            >
-                                <Text
-                                    as={
-                                        ticketType === fi.type &&
-                                        ticketID === fi._id
-                                            ? 'b'
-                                            : ''
-                                    }
-                                    fontSize="xs"
-                                    onClick={() => navigate(`${fi.path}`)}
-                                >
-                                    {`${fi.type} - ${fi.name}`}
-                                </Text>
-                            </Box>
-                            {sortTickets(fi.personalPurchases).map((ppr) => {
-                                return (
-                                    <Box
-                                        key={ppr.code}
-                                        pos="relative"
-                                        left="8"
-                                        border="1px"
-                                        borderColor="gray.200"
-                                        borderRadius="4"
-                                        cursor="pointer"
-                                        bgColor="#22f5a1"
-                                    >
-                                        <Text
-                                            as={
-                                                ticketType === ppr.type &&
-                                                ticketID === ppr._id
-                                                    ? 'b'
-                                                    : ''
-                                            }
-                                            fontSize="xs"
-                                            onClick={() =>
-                                                navigate(`${ppr.path}`)
-                                            }
-                                        >
-                                            {`${ppr.type} - ${ppr.name}`}
-                                        </Text>
-                                    </Box>
-                                )
-                            })}
-                            {sortTickets(fi.uwFinancePurchases).map((upr) => {
-                                return (
-                                    <Box
-                                        key={upr.code}
-                                        pos="relative"
-                                        left="8"
-                                        border="1px"
-                                        borderColor="gray.200"
-                                        borderRadius="4"
-                                        cursor="pointer"
-                                        bgColor="#ff87f3"
-                                    >
-                                        <Text
-                                            as={
-                                                ticketType === `${upr.type}` &&
-                                                ticketID === upr._id
-                                                    ? 'b'
-                                                    : ''
-                                            }
-                                            fontSize="xs"
-                                            onClick={() =>
-                                                navigate(`${upr.path}`)
-                                            }
-                                        >
-                                            {`${upr.type} - ${upr.name}`}
-                                        </Text>
-                                    </Box>
-                                )
-                            })}
-                        </Box>
-                    )
-                })}
+        <Stack>
+            <Box
+                bgColor="purple.200"
+                p="4px 8px"
+                mr="20%"
+                borderRadius="8px"
+                cursor="pointer"
+            >
+                <Text
+                    fontWeight={
+                        currentTicket.code === tree.code ? '800' : '600'
+                    }
+                    fontSize="xs"
+                    onClick={() => navigate(tree.path)}
+                >
+                    {`${tree.code}: ${tree.name}`}
+                </Text>
             </Box>
-        </Box>
+
+            {sortTickets(tree.fundingItems).map((fi) => {
+                return (
+                    <Box key={fi.code} m="0 !important">
+                        <Box
+                            bgColor="blue.200"
+                            m="4px 10% 0"
+                            p="4px 8px"
+                            borderRadius="8px"
+                            cursor="pointer"
+                        >
+                            <Text
+                                fontWeight={
+                                    currentTicket.code === fi.code
+                                        ? '800'
+                                        : '600'
+                                }
+                                fontSize="xs"
+                                onClick={() => navigate(`${fi.path}`)}
+                            >
+                                {`${fi.code}: ${fi.name}`}
+                            </Text>
+                        </Box>
+                        {sortTickets(fi.personalPurchases).map((ppr) => {
+                            return (
+                                <Box
+                                    key={ppr.code}
+                                    bgColor="cyan.200"
+                                    m="4px 0 0 20%"
+                                    p="4px 8px"
+                                    borderRadius="8px"
+                                    cursor="pointer"
+                                >
+                                    <Text
+                                        fontWeight={
+                                            currentTicket.code === ppr.code
+                                                ? '800'
+                                                : '600'
+                                        }
+                                        fontSize="xs"
+                                        onClick={() => navigate(`${ppr.path}`)}
+                                    >
+                                        {`${ppr.code}: ${ppr.name}`}
+                                    </Text>
+                                </Box>
+                            )
+                        })}
+                        {sortTickets(fi.uwFinancePurchases).map((upr) => {
+                            return (
+                                <Box
+                                    key={upr.code}
+                                    bgColor="teal.200"
+                                    m="4px 0 0 20%"
+                                    p="4px 8px"
+                                    borderRadius="8px"
+                                    cursor="pointer"
+                                >
+                                    <Text
+                                        fontWeight={
+                                            currentTicket.code === upr.code
+                                                ? '800'
+                                                : '600'
+                                        }
+                                        fontSize="xs"
+                                        onClick={() => navigate(`${upr.path}`)}
+                                    >
+                                        {`${upr.code}: ${upr.name}`}
+                                    </Text>
+                                </Box>
+                            )
+                        })}
+                    </Box>
+                )
+            })}
+        </Stack>
     )
 }
 
