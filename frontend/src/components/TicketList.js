060290bc8108ff254d6380d9a908803ff0b2470e
Anson He <60114875+ansonjwhe@users.noreply.github.com>
2023-05-21 12:39:21 -0400

    Misc bugfixes and refactor ticket tree (#100)
    - Add loading spinner to ticket list
    - move TICKET_TYPES to constants file
    - abstract ticketTree state to dashboard, handle WATO Cash special case
    - bugfix: redirect to /notfound if ticket specified in URL does not
    exist
    - bugfix: set currentTicket id as int not str when extracting from URL

--- frontend/src/components/TicketList.js
+++ frontend/src/components/TicketList.js
@@ -50,22 +50,21 @@ const TicketList = ({ allTickets }) => {
             <FilterDropdown filter={filter} setFilter={setFilter} />
             <VStack w="100%" h="calc(100vh - 152px)" overflowY="scroll">
                 {filteredTickets.map((ticket) => {
-                    const code = `${ticket.type}-${ticket.id}`
                     return (
                         <Card
-                            key={code}
+                            key={ticket.code}
                             w="100%"
                             borderTop="1px solid #dedede"
                             borderBottom="1px solid #dedede"
                             borderRadius="0"
                             mt="0 !important"
                             onClick={() =>
-                                navigate(`/${ticket.type}/${ticket.id}`)
+                                navigate(ticket.path)
                             }
                             cursor="pointer"
                         >
                             <CardBody p="8px 16px">
-                                <Text>{code}</Text>
+                                <Text>{ticket.code}</Text>
                                 <Text noOfLines="1">{ticket.name}</Text>
                             </CardBody>
                         </Card>
