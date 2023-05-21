c4734988495a34c60ee8814e780367a63cd25c25
Anson He <ansonjwhe@gmail.com>
2023-05-21 12:32:21 -0400

    return early spinner in ticket list if allTickets is empty

--- frontend/src/components/TicketList.js
+++ frontend/src/components/TicketList.js
@@ -15,6 +15,25 @@ const TicketList = ({ allTickets }) => {
         TICKET_TYPES.UPR,
     ])
 
+    if (
+        Object.keys(TICKET_TYPES)
+            .map((type) => allTickets[type])
+            .flat().length === 0
+    ) {
+        return (
+            <Box minW={{ base: '200px', lg: '280px', xl: '300px' }}>
+                <FilterDropdown filter={filter} setFilter={setFilter} />
+                <Center
+                    w="100%"
+                    h="calc(100vh - 152px)"
+                    border="1px solid #dedede"
+                >
+                    <LoadingSpinner />
+                </Center>
+            </Box>
+        )
+    }
+
     const filteredTickets = Object.entries(allTickets)
         .map(([ticketType, collection]) => {
             return collection.map((ticket) => ({
@@ -26,54 +45,33 @@ const TicketList = ({ allTickets }) => {
         .flat()
         .filter((ticket) => filter.includes(ticket.type))
 
-    const isAllTicketsEmpty =
-        Object.keys(TICKET_TYPES)
-            .map((type) => allTickets[type])
-            .flat().length === 0
-
     return (
         <Box minW={{ base: '200px', lg: '280px', xl: '300px' }}>
             <FilterDropdown filter={filter} setFilter={setFilter} />
-
-            {isAllTicketsEmpty ? (
-                <Center
-                    w="100%"
-                    h="calc(100vh - 152px)"
-                    border="1px solid #dedede"
-                >
-                    <LoadingSpinner />
-                </Center>
-            ) : (
-                <VStack
-                    pos="relative"
-                    w="100%"
-                    h="calc(100vh - 152px)"
-                    overflowY="scroll"
-                >
-                    {filteredTickets.map((ticket) => {
-                        const code = `${ticket.type}-${ticket.id}`
-                        return (
-                            <Card
-                                key={code}
-                                w="100%"
-                                borderTop="1px solid #dedede"
-                                borderBottom="1px solid #dedede"
-                                borderRadius="0"
-                                mt="0 !important"
-                                onClick={() =>
-                                    navigate(`/${ticket.type}/${ticket.id}`)
-                                }
-                                cursor="pointer"
-                            >
-                                <CardBody p="8px 16px">
-                                    <Text>{code}</Text>
-                                    <Text noOfLines="1">{ticket.name}</Text>
-                                </CardBody>
-                            </Card>
-                        )
-                    })}
-                </VStack>
-            )}
+            <VStack w="100%" h="calc(100vh - 152px)" overflowY="scroll">
+                {filteredTickets.map((ticket) => {
+                    const code = `${ticket.type}-${ticket.id}`
+                    return (
+                        <Card
+                            key={code}
+                            w="100%"
+                            borderTop="1px solid #dedede"
+                            borderBottom="1px solid #dedede"
+                            borderRadius="0"
+                            mt="0 !important"
+                            onClick={() =>
+                                navigate(`/${ticket.type}/${ticket.id}`)
+                            }
+                            cursor="pointer"
+                        >
+                            <CardBody p="8px 16px">
+                                <Text>{code}</Text>
+                                <Text noOfLines="1">{ticket.name}</Text>
+                            </CardBody>
+                        </Card>
+                    )
+                })}
+            </VStack>
         </Box>
     )
 }
