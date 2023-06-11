24d9e4afd6abdf6a67b81c324c3d472e089d197b
Anson He <ansonjwhe@gmail.com>
2023-06-11 18:36:32 -0400

    remove unused email automation (out of scope)

--- backend/service/automation.js
+++ backend/service/automation.js
@@ -1,21 +1,12 @@
-import { PersonalPurchaseApproved } from '../emails/emails'
-import { createEmail } from './emails.controller'
+// import { PersonalPurchaseApproved } from '../emails/emails'
+// import { createEmail } from './createEmail'
 
 // ticket of type personalPurchase or uwFinancePurchase
-export const transitionReadyToBuy = async (ticketType, ticketId) => {
+const transitionReadyToBuy = async (ticketType, ticketId) => {
     const ticket = ticketType.findByIdAndUpdate(ticketId, {
         status: 'READY_TO_BUY',
     })
-    // const payload = PersonalPurchaseApproved({
-    //     ticket.
-
-    // })
-    // // send email
-    // createEmail(
-    //     {
-
-    //     }
-    // )
+    return ticket
 }
 
 module.exports = {
