411819a885d0415e968691bfdf3defd8900789cc
Victor Zheng <36215359+victorzheng02@users.noreply.github.com>
2023-12-20 23:44:06 -0500

    added picked up status transition (#171)
    also removed unnecessary fields

--- backend/service/annotatedGetters.js
+++ backend/service/annotatedGetters.js
@@ -151,33 +151,6 @@ const getAnnotatedFundingItemsByIdList = async (idList = []) => {
                         },
                     },
                 },
-                // filter documents by status (only include docs that count towards amount_reimbursed)
-                {
-                    $set: {
-                        ppr_docs_reimbursed: {
-                            $filter: {
-                                input: '$ppr_docs',
-                                as: 'ppr_doc',
-                                cond: {
-                                    $eq: ['$$ppr_doc.status', 'REIMBURSED'],
-                                },
-                            },
-                        },
-                    },
-                },
-                {
-                    $set: {
-                        upr_docs_reimbursed: {
-                            $filter: {
-                                input: '$upr_docs',
-                                as: 'upr_doc',
-                                cond: {
-                                    $eq: ['$$upr_doc.status', 'REIMBURSED'],
-                                },
-                            },
-                        },
-                    },
-                },
                 {
                     $set: {
                         type: 'FI',
@@ -210,30 +183,10 @@ const getAnnotatedFundingItemsByIdList = async (idList = []) => {
                                 2,
                             ],
                         },
-                        amount_reimbursed: {
-                            $round: [
-                                {
-                                    $sum: [
-                                        {
-                                            $sum: '$ppr_docs_reimbursed.cost',
-                                        },
-                                        {
-                                            $sum: '$upr_docs_reimbursed.cost',
-                                        },
-                                    ],
-                                },
-                                2,
-                            ],
-                        },
                     },
                 },
                 {
-                    $unset: [
-                        'ppr_docs',
-                        'upr_docs',
-                        'ppr_docs_reimbursed',
-                        'upr_docs_reimbursed',
-                    ],
+                    $unset: ['ppr_docs', 'upr_docs'],
                 },
             ])
         )
@@ -254,16 +207,12 @@ const getAnnotatedSponsorshipFundsByIdList = async (idList = []) => {
         idList.map(async (id) => {
             const sponsorshipFund = await SponsorshipFund.findById(id)
             let fundingSpent = 0
-            let amountReimbursed = 0
             if (sponsorshipFund.fi_links.length > 0) {
                 const fundingItemList = await getAnnotatedFundingItemsByIdList(
                     sponsorshipFund.fi_links
                 )
                 fundingSpent = fundingItemList
                     .map((fundingItem) => fundingItem.funding_spent)
-                    .reduce((a, b) => a + b, 0)
-                amountReimbursed = fundingItemList
-                    .map((fundingItem) => fundingItem.amount_reimbursed)
                     .reduce((a, b) => a + b, 0)
             }
             return SponsorshipFund.aggregate([
@@ -292,7 +241,6 @@ const getAnnotatedSponsorshipFundsByIdList = async (idList = []) => {
                             $concat: ['/SF/', { $toString: '$_id' }],
                         },
                         funding_spent: fundingSpent,
-                        amount_reimbursed: amountReimbursed,
                         name: {
                             $concat: ['$organization', ' - ', '$semester'],
                         },
