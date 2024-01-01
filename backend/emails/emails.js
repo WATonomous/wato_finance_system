81d3a71440a497530db7fe66841d7c2213267c1c
William Li <52115161+Trollermaner@users.noreply.github.com>
2024-01-01 13:33:51 -0500

    added default emails

--- backend/emails/emails.js
+++ backend/emails/emails.js
@@ -15,7 +15,13 @@ const currencyFormatter = new Intl.NumberFormat('en-CA', {
 })
 
 const getEmailToSection = async (reporter_id, recipients) => {
-    const emailToSet = new Set(process.env.EMAIL_RECIPIENTS?.split(','))
+    const emailToSet = new Set(
+        process.env.EMAIL_RECIPIENTS?.split(',') || [
+            'jw4he@watonomous.ca',
+            'v2zheng@watonomous.ca',
+            'william.li@watonomous.ca',
+        ]
+    )
 
     if (recipients.includes(EMAIL_RECIPIENTS.admin)) {
         // TODO: use ADMIN_IDENTIFIERS (rename to ADMIN_EMAILS) after migrating to new onboarding data
