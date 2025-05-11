907d7e4043503b0bf2d3ac002a475f81a3041316
Jimmy Jiang <71370434+Jimmyj30@users.noreply.github.com>
2025-05-10 21:22:26 -0600

    Initial vcluster provisioning scripts (#205)
    TODO:
    - See how this works with the "synced" external resources
    - edit: external resources are recreated, when deleted from inside the
    vcluster, so this should be fine:
    (https://www.vcluster.com/docs/vcluster/next/configure/vcluster-yaml/sync/from-host/secrets#:~:text=When%20you%20delete,corresponding%20virtual%20object.)
    - we will refer to those synced external resources using terraform data
    blocks
    
    ---------
    
    Co-authored-by: Ben Zhang <5977478+ben-z@users.noreply.github.com>

---
+++ deployment/terraform/main.tf
@@ -0,0 +1,63 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "wato-finance"
+
+    workspaces {
+      name = "watonomous-finance-system"
+    }
+  }
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.25.2"
+    }
+  }
+}
+
+variable "kube_config_path" {
+  type = string
+  description = "The path to the kubeconfig file"
+}
+
+provider "kubernetes" {
+  config_path = var.kube_config_path
+}
+
+locals {
+  backend_hostname = "finance-api.watonomous.ca"
+  backend_app_name = "wato-finance-backend"
+  backend_env_volume_name = "backend-env"
+  user_directory_volume_name = "user-directory"
+  backend_app_port = 5000
+  backend_allowed_origins = [
+    "https://cloud.watonomous.ca",
+    "https://rgw-preview.watonomous.ca",
+    "http://localhost:3000", # local dev
+    "https://finance-frontend.watonomous.ca",
+    "https://*.use.devtunnels.ms", # vscode tunnel
+  ]
+
+  frontend_hostname = "finance-frontend.watonomous.ca"
+  frontend_app_name = "wato-finance-frontend"
+  frontend_env_volume_name = "frontend-env"
+  frontend_app_port = 3000
+}
+
+// Synced with the vcluster from the host:
+// https://www.vcluster.com/docs/vcluster/configure/vcluster-yaml/sync/from-host
+data "kubernetes_namespace" "wato_finance_backend" {
+  metadata {
+    name = "wato-finance-backend"
+  }
+}
+
+// Synced with the vcluster from the host:
+// https://www.vcluster.com/docs/vcluster/configure/vcluster-yaml/sync/from-host
+data "kubernetes_secret" "user_directory_json" {
+  metadata {
+    name = "user-directory-json"
+    namespace = data.kubernetes_namespace.wato_finance_backend.metadata[0].name
+  }
+}
