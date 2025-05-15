f796849567b33b40ca431558722aed5159c8db87
Jimmy Jiang <j257jian@uwaterloo.ca>
2025-05-15 16:39:03 -0600

    provision k8s resources except deployments - TODO: try to work it out later

--- deployment/terraform/backend.tf
+++ deployment/terraform/backend.tf
@@ -33,89 +33,89 @@ resource "kubernetes_secret" "backend_env_vars" {
 
 // Deployment
 
-resource "kubernetes_deployment" "wato_finance_backend_deployment" {
-  metadata {
-    name        = local.backend_app_name
-    namespace   = data.kubernetes_namespace.wato_finance_backend.metadata[0].name
-    annotations = {
-      "reloader.stakater.com/auto" = "true"
-    }
-  }
-  spec {
-    replicas = 1
-    selector {
-      match_labels = {
-        app = local.backend_app_name
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          app = local.backend_app_name
-        }
-      }
-      spec {
-        container {
-          image = local.backend_image
-          name  = local.backend_app_name
-          image_pull_policy = "Always"
-          port { 
-            container_port = local.backend_app_port
-          } 
-          # dynamic `for_each` can't work with sensitive values: https://github.com/hashicorp/terraform/issues/29744
-          # Note that no sensitive data is shown here, volume_mount.key is non-sensitive
-          dynamic "volume_mount" {
-            for_each = nonsensitive(kubernetes_secret.backend_env_vars.data)
-            content {
-              name       = local.backend_env_volume_name
-              mount_path = join("", ["/usr/src/app/backend/", volume_mount.key])
-              sub_path   = volume_mount.key
-              read_only  = true
-            }
-          }
-          volume_mount { // volume mount for user directory
-             name       = local.user_directory_volume_name
-             mount_path = "/usr/src/app/backend/user_directory.json"
-             sub_path   = "user_directory.json"
-             read_only  = true
-          }
-          // TODO: is this even needed?
-          security_context {
-            allow_privilege_escalation = false
-            run_as_non_root            = false
-            capabilities {
-              drop = ["ALL"]
-            }
-            seccomp_profile {
-              type = "RuntimeDefault"
-            }
-          }
-        }
-        volume { # volume containing .env file and service account key
-          name = local.backend_env_volume_name
-          secret {
-            secret_name = kubernetes_secret.backend_env_vars.metadata[0].name
-          }
-        }
-        volume { # volume containing user directory
-          name = local.user_directory_volume_name
-          secret {
-            secret_name = data.kubernetes_secret.user_directory_json.metadata[0].name
-          }
-        }
-        toleration { # Allow scheduling on spot nodes.
-          key = "kubernetes.azure.com/scalesetpriority"
-          operator = "Equal"
-          value = "spot"
-          effect = "NoSchedule"
-        }
-      }
-    }
-  }
+# resource "kubernetes_deployment" "wato_finance_backend_deployment" {
+#   metadata {
+#     name        = local.backend_app_name
+#     namespace   = data.kubernetes_namespace.wato_finance_backend.metadata[0].name
+#     annotations = {
+#       "reloader.stakater.com/auto" = "true"
+#     }
+#   }
+#   spec {
+#     replicas = 1
+#     selector {
+#       match_labels = {
+#         app = local.backend_app_name
+#       }
+#     }
+#     template {
+#       metadata {
+#         labels = {
+#           app = local.backend_app_name
+#         }
+#       }
+#       spec {
+#         container {
+#           image = local.backend_image
+#           name  = local.backend_app_name
+#           image_pull_policy = "Always"
+#           port { 
+#             container_port = local.backend_app_port
+#           } 
+#           # dynamic `for_each` can't work with sensitive values: https://github.com/hashicorp/terraform/issues/29744
+#           # Note that no sensitive data is shown here, volume_mount.key is non-sensitive
+#           dynamic "volume_mount" {
+#             for_each = nonsensitive(kubernetes_secret.backend_env_vars.data)
+#             content {
+#               name       = local.backend_env_volume_name
+#               mount_path = join("", ["/usr/src/app/backend/", volume_mount.key])
+#               sub_path   = volume_mount.key
+#               read_only  = true
+#             }
+#           }
+#           volume_mount { // volume mount for user directory
+#              name       = local.user_directory_volume_name
+#              mount_path = "/usr/src/app/backend/user_directory.json"
+#              sub_path   = "user_directory.json"
+#              read_only  = true
+#           }
+#           // TODO: is this even needed?
+#           security_context {
+#             allow_privilege_escalation = false
+#             run_as_non_root            = false
+#             capabilities {
+#               drop = ["ALL"]
+#             }
+#             seccomp_profile {
+#               type = "RuntimeDefault"
+#             }
+#           }
+#         }
+#         volume { # volume containing .env file and service account key
+#           name = local.backend_env_volume_name
+#           secret {
+#             secret_name = kubernetes_secret.backend_env_vars.metadata[0].name
+#           }
+#         }
+#         volume { # volume containing user directory
+#           name = local.user_directory_volume_name
+#           secret {
+#             secret_name = data.kubernetes_secret.user_directory_json.metadata[0].name
+#           }
+#         }
+#         toleration { # Allow scheduling on spot nodes.
+#           key = "kubernetes.azure.com/scalesetpriority"
+#           operator = "Equal"
+#           value = "spot"
+#           effect = "NoSchedule"
+#         }
+#       }
+#     }
+#   }
 
-  lifecycle {
-    ignore_changes = [ 
-      spec[0].template[0].metadata[0].annotations["reloader.stakater.com/last-reloaded-from"]
-    ]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [ 
+#       spec[0].template[0].metadata[0].annotations["reloader.stakater.com/last-reloaded-from"]
+#     ]
+#   }
+# }
--- deployment/terraform/frontend.tf
+++ deployment/terraform/frontend.tf
@@ -34,90 +34,90 @@ resource "kubernetes_secret" "frontend_env_vars" {
 
 // Deployment
 
-resource "kubernetes_deployment" "wato_finance_frontend_deployment" {
-  metadata {
-    name        = local.frontend_app_name
-    namespace   = kubernetes_namespace.wato_finance_frontend.metadata[0].name
-    annotations = {
-      "reloader.stakater.com/auto" = "true"
-    }
-  }
-  spec {
-    replicas = 1
-    selector {
-      match_labels = {
-        app = local.frontend_app_name
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          app = local.frontend_app_name
-        }
-      }
-      spec {
-        container {
-          image = local.frontend_image
-          name  = local.frontend_app_name
-          image_pull_policy = "Always"
-          port { 
-            container_port = local.frontend_app_port
-          } 
-          volume_mount {
-            name       = local.frontend_env_volume_name
-            mount_path = "/usr/src/app/frontend/.env"
-            sub_path   = ".env"
-            read_only  = true
-          }
-          liveness_probe {
-            http_get {
-              path = "/"
-              port = local.frontend_app_port
-            }
-            period_seconds    = 10
-            timeout_seconds   = 5
-            failure_threshold = 3
-          }
-          startup_probe {
-            http_get {
-              path = "/"
-              port = local.frontend_app_port
-            }
-            period_seconds        = 10
-            timeout_seconds       = 5
-            failure_threshold     = 12
-          }
-          // TODO: is this even needed?
-          security_context {
-            allow_privilege_escalation = false
-            run_as_non_root            = false
-            capabilities {
-              drop = ["ALL"]
-            }
-            seccomp_profile {
-              type = "RuntimeDefault"
-            }
-          }
-        }
-        volume {
-          name = local.frontend_env_volume_name
-          secret {
-            secret_name = kubernetes_secret.frontend_env_vars.metadata[0].name
-          }
-        }
-        toleration { # Allow scheduling on spot nodes.
-          key = "kubernetes.azure.com/scalesetpriority"
-          operator = "Equal"
-          value = "spot"
-          effect = "NoSchedule"
-        }
-      }
-    }
-  }
+# resource "kubernetes_deployment" "wato_finance_frontend_deployment" {
+#   metadata {
+#     name        = local.frontend_app_name
+#     namespace   = kubernetes_namespace.wato_finance_frontend.metadata[0].name
+#     annotations = {
+#       "reloader.stakater.com/auto" = "true"
+#     }
+#   }
+#   spec {
+#     replicas = 1
+#     selector {
+#       match_labels = {
+#         app = local.frontend_app_name
+#       }
+#     }
+#     template {
+#       metadata {
+#         labels = {
+#           app = local.frontend_app_name
+#         }
+#       }
+#       spec {
+#         container {
+#           image = local.frontend_image
+#           name  = local.frontend_app_name
+#           image_pull_policy = "Always"
+#           port { 
+#             container_port = local.frontend_app_port
+#           } 
+#           volume_mount {
+#             name       = local.frontend_env_volume_name
+#             mount_path = "/usr/src/app/frontend/.env"
+#             sub_path   = ".env"
+#             read_only  = true
+#           }
+#           liveness_probe {
+#             http_get {
+#               path = "/"
+#               port = local.frontend_app_port
+#             }
+#             period_seconds    = 10
+#             timeout_seconds   = 5
+#             failure_threshold = 3
+#           }
+#           startup_probe {
+#             http_get {
+#               path = "/"
+#               port = local.frontend_app_port
+#             }
+#             period_seconds        = 10
+#             timeout_seconds       = 5
+#             failure_threshold     = 12
+#           }
+#           // TODO: is this even needed?
+#           security_context {
+#             allow_privilege_escalation = false
+#             run_as_non_root            = false
+#             capabilities {
+#               drop = ["ALL"]
+#             }
+#             seccomp_profile {
+#               type = "RuntimeDefault"
+#             }
+#           }
+#         }
+#         volume {
+#           name = local.frontend_env_volume_name
+#           secret {
+#             secret_name = kubernetes_secret.frontend_env_vars.metadata[0].name
+#           }
+#         }
+#         toleration { # Allow scheduling on spot nodes.
+#           key = "kubernetes.azure.com/scalesetpriority"
+#           operator = "Equal"
+#           value = "spot"
+#           effect = "NoSchedule"
+#         }
+#       }
+#     }
+#   }
 
-  lifecycle {
-    ignore_changes = [ 
-      spec[0].template[0].metadata[0].annotations["reloader.stakater.com/last-reloaded-from"]
-    ]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [ 
+#       spec[0].template[0].metadata[0].annotations["reloader.stakater.com/last-reloaded-from"]
+#     ]
+#   }
+# }
